d2257eb1d5cd51f9fa4dd85273ffa1c4a3357639
Paulo02-pixel <u20191e562@upc.edu.pe>
2025-05-16 06:11:33 -0500

    Create home-member

--- src/app/member/pages/home-member/home-member.component.ts
+++ src/app/member/pages/home-member/home-member.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import {CurrencyPipe, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-home-member',
-  imports: [],
   templateUrl: './home-member.component.html',
-  styleUrl: './home-member.component.css'
+  imports: [
+    CurrencyPipe,
+    NgIf
+  ],
+  styleUrls: ['./home-member.component.css']
 })
 export class HomeMemberComponent {
+  memberName = 'Ana López';
+  householdName = 'Sunny Apartments';
+  memberContribution = 120.5;
+  joinDate = '2023-08-15';
+  lastBill = {
+    description: 'Electricity - March',
+    amount: 40.75
+  };
 
+  constructor(private router: Router) {}
+
+  viewContributionHistory() {
+    this.router.navigate(['/member/contributions']);
+  }
+
+  viewBills() {
+    this.router.navigate(['/member/bills']);
+  }
+
+  contactRepresentative() {
+    this.router.navigate(['/member/contact-rep']);
+  }
 }
