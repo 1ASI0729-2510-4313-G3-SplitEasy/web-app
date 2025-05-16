d00477f145070c73d8fe2ea5888f07f1935eab1c
Paulo02-pixel <u20191e562@upc.edu.pe>
2025-05-16 06:26:06 -0500

    Create contributions-member

--- src/app/member/pages/contributions-member/contributions-member.component.ts
+++ src/app/member/pages/contributions-member/contributions-member.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import {CurrencyPipe, DatePipe, NgForOf} from '@angular/common';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-contributions-member',
-  imports: [],
+  imports: [
+    CurrencyPipe,
+    DatePipe,
+    NgForOf
+  ],
   templateUrl: './contributions-member.component.html',
   styleUrl: './contributions-member.component.css'
 })
 export class ContributionsMemberComponent {
+  totalContributed = 150.00;
+  assignedGoal = 200.00;
+  deadline = new Date('2025-06-15');
+  status = 'In Progress';
 
+  contributionHistory = [
+    { date: new Date('2025-05-01'), amount: 50.00, comment: 'Monthly payment', status: 'Approved' },
+    { date: new Date('2025-04-01'), amount: 100.00, comment: 'Initial contribution', status: 'Approved' }
+  ];
+
+  constructor(private router: Router) {}
+
+  addContribution() {
+    this.router.navigate(['/member/contributions/new']);
+  }
 }
